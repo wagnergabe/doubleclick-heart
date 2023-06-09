b4c002506029e86d924ce45d30365ade552f5825
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-06-08 23:21:33 -0500

    heart function and count setup completed

--- script.js
+++ script.js
@@ -2,15 +2,40 @@ const loved = document.querySelector('.loved');
 const times = document.querySelector('#times');
 
 let clickTime = 0;
+let timesClicked = 0;
 
 loved.addEventListener('click', (e) => {
     if(clickTime === 0) {
         clickTime = new Date().getTime()
     } else {
         if((new Date().getTime() - clickTime) < 800) {
-            console.log('test')
+            createHeart(e) 
+            clickTime = 0
         } else {
             clickTime = new Date().getTime()
         }
     }
-})
+});
+
+const createHeart = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fa-solid');
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX 
+    const y = e.clientY 
+
+
+    const leftOffset = e.target.offSetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loved.appendChild(heart);
+
+    times.innerHTML = ++timesClicked;
+}
